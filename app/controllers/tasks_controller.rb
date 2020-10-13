8c984f7b487bba4efd961d317a2dcf4c458bb219
leena khalili <engleena@yahoo.com>
2020-10-13 14:54:44 +0300

    created CRUD with rails, love it

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
@@ -14,10 +16,32 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
+
+    redirect_to task_path(@task)
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+
+    redirect_to task_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params["id"])
   end
 
   def task_params
-    params.require(:task).permit(:name, :address, :rating)
+    params.require(:task).permit(:title, :details, :completed)
   end
-
 end
